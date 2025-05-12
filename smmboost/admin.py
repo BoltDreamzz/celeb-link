1ba265e52bfd8afd9615a7cd9ee34780532c673e
BoltDreamzz <boltdreamz@gmail.com>
2025-05-12 09:58:37 -0700

    Added topup

--- smmboost/admin.py
+++ smmboost/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Wallet, Service, Order, WalletTransaction,
 
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(Order)
+admin.site.register(Wallet)
+admin.site.register(WalletTransaction)
+# from django.contrib.auth import get_user_model
