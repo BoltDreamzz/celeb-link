9652cf1b559596422b9893b15ace13af0518aa39
BoltDreamzz <boltdreamz@gmail.com>
2025-05-12 08:44:30 -0700

    Added topup

--- smmboost/forms.py
+++ smmboost/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from .models import Service
 
 class OrderForm(forms.Form):
-    service = forms.ModelChoiceField(
-        queryset=Service.objects.all(),
-        widget=forms.Select(attrs={
-            'class': 'w-full p-2 border rounded-lg border-gray-300 focus:ring focus:outline-none'
-        })
-    )
+    # service = forms.ModelChoiceField(
+    #     queryset=Service.objects.all(),
+    #     widget=forms.Select(attrs={
+    #         'class': 'w-full p-2 border rounded-lg border-gray-300 focus:ring focus:outline-none'
+    #     })
+    # )
     link = forms.URLField(
         widget=forms.URLInput(attrs={
             'placeholder': 'https://example.com/UserName',
